4b1203193e18d3ee6606dc486ef2c588289fd283
wasabi315 <wasabi.nvim.st@gmail.com>
2021-09-12 23:32:41 +0900

    Fix date format

--- src/stringify.ts
+++ src/stringify.ts
@@ -1,25 +1,42 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-export function toJsonString<T extends Record<any, any>>(xs: T[]): string {
+type Table = Record<string, string | number | Date>;
+
+export function toJsonString<T extends Table>(xs: T[]): string {
   return JSON.stringify(xs, null, 2);
 }
 
-export function toCsvString<T extends Record<any, any>>(xs: T[]): string {
+export function toCsvString<T extends Table>(xs: T[]): string {
   if (xs.length === 0) {
     return '';
   }
 
-  const header = [...Object.keys(xs[0])];
-  const rows = xs.map((x) => [...Object.values(x)].map(String));
-  return [header, ...rows].map((row) => row.join(',')).join('\n');
+  return withSettingDateFormatISO(() => {
+    const header = [...Object.keys(xs[0])];
+    const rows = xs.map((x) => [...Object.values(x)].map(String));
+    return [header, ...rows].map((row) => row.join(',')).join('\n');
+  });
 }
 
-export function toMdTableString<T extends Record<any, any>>(xs: T[]): string {
+export function toMdTableString<T extends Table>(xs: T[]): string {
   if (xs.length === 0) {
     return '';
   }
 
-  const header = [...Object.keys(xs[0])];
-  const rows = xs.map((x) => [...Object.values(x)].map(String));
-  return [header, ...rows].map((row) => `|${row.join('|')}|`).join('\n');
+  return withSettingDateFormatISO(() => {
+    const header = [...Object.keys(xs[0])];
+    const rows = xs.map((x) => [...Object.values(x)].map(String));
+    return [header, ...rows].map((row) => `|${row.join('|')}|`).join('\n');
+  });
+}
+
+// 一時的にDate.toStringをtoISOStringにすり替える
+function withSettingDateFormatISO<T>(f: () => T): T {
+  const toString = Date.prototype.toString;
+  Date.prototype.toString = Date.prototype.toISOString;
+  try {
+    return f();
+  } finally {
+    Date.prototype.toString = toString;
+  }
 }
