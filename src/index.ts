fe7c184e25399d66cad2c931e2d055e109736d14
wasabi315 <wasabi.nvim.st@gmail.com>
2021-09-14 23:52:11 +0900

    Change CSV format

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
-import { extractRooms } from './extract';
+import { extractRooms, Room } from './extract';
 import { downloadTextAs, copyTextToClipboard } from './save';
 import { toCsvString, toMdTableString } from './stringify';
+
+function toGoogleCalendarData(room: Room) {
+  const dateString = (date: Date) =>
+    new Intl.DateTimeFormat('en-US').format(date);
+  const timeString = (date: Date) =>
+    new Intl.DateTimeFormat('en-US', { timeStyle: 'short' }).format(date);
+
+  return {
+    Subject: '進捗部屋',
+    Location: room.place,
+    'Start Date': dateString(room.timeStart),
+    'End Date': dateString(room.timeEnd),
+    'Start Time': timeString(room.timeStart),
+    'End Time': timeString(room.timeEnd),
+  };
+}
 
 async function main() {
   const rooms = await extractRooms();
@@ -16,7 +32,7 @@ async function main() {
   }
 
   if (window.confirm('CSVをダウンロードしますか?')) {
-    const text = toCsvString(rooms);
+    const text = toCsvString(rooms.map(toGoogleCalendarData));
     downloadTextAs('cal.csv', text, { type: 'text/csv' });
   }
 }
