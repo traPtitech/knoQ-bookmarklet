73fdd322cb7487bcc7d09587a131be7299f0b688
wasabi315 <wasabi.nvim.st@gmail.com>
2022-06-18 01:04:09 +0900

    Fix

--- src/extract.ts
+++ src/extract.ts
@@ -8,18 +8,18 @@ export type Room = {
 
 // 講義室予約ページから4週間分のtraPの施設予約を抽出する
 export async function extractRooms(): Promise<Room[]> {
-  await loadWeeklyReservations();
-
   const rooms: Room[] = [];
-  for (let i = 0; i < 4; i++) {
-    rooms.push(...extractRoomsOneWeek());
+
+  await loadWeeklyReservations();
+  rooms.push(...extractRoomsOneWeek());
+  for (let i = 0; i < 3; i++) {
     await loadNextWeekReservations();
+    rooms.push(...extractRoomsOneWeek());
   }
 
-  // timeStartの昇順でソート
-  rooms.sort((a, b) => a.timeStart.getTime() - b.timeStart.getTime());
-
-  return rooms;
+  return rooms
+    .filter(({ timeStart }) => new Date().getTime() < timeStart.getTime())
+    .sort((a, b) => a.timeStart.getTime() - b.timeStart.getTime());
 }
 
 // 施設予約ページで週間表示の講義室予約を表示させる
@@ -29,7 +29,9 @@ async function loadWeeklyReservations(): Promise<void> {
 
   // 講義室[大岡山]ボタンをクリック
   document
-    .querySelector<HTMLElement>('#tbl2 > tbody > tr > td:nth-child(3) > a')!
+    .querySelector<HTMLElement>(
+      '#tbl2 > tbody > tr:nth-child(2) > td:nth-child(1) > a',
+    )!
     .click();
 
   await waitForLoading();
@@ -48,56 +50,67 @@ async function loadNextWeekReservations(): Promise<void> {
 }
 
 // 講義室情報が表示されるまで待つ
-async function waitForLoading(): Promise<void> {
-  for (let limit = 20; limit--; ) {
-    await new Promise((r) => setTimeout(r, 500));
-
-    // ロード中の時は#divTblの子がimgになる
-    const loaded =
-      document.querySelector('#divTbl')?.firstElementChild?.tagName === 'DIV';
-    if (loaded) {
-      return;
-    }
-  }
-
-  throw new Error('講義室情報がロードされませんでした');
+function waitForLoading(): Promise<void> {
+  return new Promise((resolve) => {
+    const target = document.querySelector('#divTbl')!;
+    const observer = new MutationObserver((mutationList, observer) => {
+      for (const mutation of mutationList) {
+        if (mutation.type !== 'childList') {
+          continue;
+        }
+        if (mutation.addedNodes[1]?.nodeName === 'DIV') {
+          observer.disconnect();
+          resolve();
+          return;
+        }
+      }
+    });
+    observer.observe(target, { childList: true });
+  });
 }
 
 // 週表示の講義室予約ページから1週間分のtraPの施設予約を抽出する
 function extractRoomsOneWeek(): Room[] {
-  const roomRows = document.querySelectorAll<HTMLTableRowElement>(
-    'table.tblMonth > tbody > tr.trRoom',
+  // traPの枠のリストを取得
+  const elIter = document.evaluate(
+    '//span[contains(., "traP")]',
+    document,
+    null,
+    XPathResult.UNORDERED_NODE_ITERATOR_TYPE,
+    null,
   );
-  return Array.from(roomRows).flatMap(extractRoomsOfRow);
+
+  const rooms: Room[] = [];
+
+  for (
+    let el = elIter.iterateNext() as HTMLElement | null;
+    el !== null;
+    el = elIter.iterateNext() as HTMLElement | null
+  ) {
+    const td = el.closest('td')!;
+    const tr = td.closest('tr')!;
+
+    const place = (
+      tr.children[tr.classList.contains('trTop') ? 1 : 0] as HTMLElement
+    ).innerText;
+    // YYYYMMDD
+    const date = td.dataset['date']!;
+    // HH:mm
+    const [, start, end] = el.innerText.match(/(\d\d:\d\d) - (\d\d:\d\d)/)!;
+
+    rooms.push({
+      place,
+      timeStart: createDate(date, start),
+      timeEnd: createDate(date, end),
+    });
+  }
+
+  return rooms;
 }
 
-// 週表示の講義室予約表の行から1週間分のtraPの施設予約を抽出する
-function extractRoomsOfRow(row: HTMLTableRowElement): Room[] {
-  const roomName = row.classList.contains('trTop')
-    ? row.cells[1].innerText.trim()
-    : row.cells[0].innerText.trim();
-  const cells = row.querySelectorAll('td');
-  return Array.from(cells).flatMap((cell) =>
-    extractRoomOfCell(cell).map((room) => ({ place: roomName, ...room })),
-  );
-}
-
-// 週表示の講義室予約の表のマスからtraPの施設予約情報を抽出する
-function extractRoomOfCell(cell: HTMLTableCellElement): Omit<Room, 'place'>[] {
-  let date = cell.dataset.date!; // yyyymmdd;
-  date = `${date.slice(0, 4)}-${date.slice(4, 6)}-${date.slice(6, 8)}`; // yyyy-mm-dd
-
-  const cards = cell.querySelectorAll<HTMLSpanElement>('span > span');
-
-  return Array.from(cards).flatMap(({ innerText: text }) => {
-    if (!/デジタル創作同好会traP/.test(text)) {
-      return [];
-    }
-
-    const [, timeStart, timeEnd] = text.match(/\((\d\d:\d\d).*(\d\d:\d\d)\)/)!; // hh:mm
-    return {
-      timeStart: new Date(`${date}T${timeStart}:00`),
-      timeEnd: new Date(`${date}T${timeEnd}:00`),
-    };
-  });
+function createDate(date: string, time: string): Date {
+  const year = date.slice(0, 4);
+  const month = date.slice(4, 6);
+  const day = date.slice(6, 8);
+  return new Date(`${year}-${month}-${day}T${time}:00+09:00`);
 }
